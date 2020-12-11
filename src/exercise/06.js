6ecc8681abc1aa0fb2ab94c654960d406494b6ab
Maxine Tillman <maxltillman@gmail.com>
2020-12-11 13:11:49 +0000

    setting username to lowercase no matter client input

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,31 +4,30 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const [error, setError] = React.useState(null)
+  const [username, setUsername] = React.useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
-    const value = event.target.elements.usernameInput.value
-    onSubmitUsername(value)
+    onSubmitUsername(username)
   }
 
   function handleChange(event) {
-    //pull value out of event.target
     const {value} = event.target
-    const isLowerCase = value === value.toLowerCase()
-    setError(isLowerCase ? null : 'Username must be lowercase')
+    setUsername(value.toLowerCase())
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" onChange={handleChange} />
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
       </div>
-      <div style={{color: 'red'}}>{error}</div>
-      <button disabled={Boolean(error)} type="submit">
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
